d91070018c003aea321925511dec3fa2ca12dc89
Yevhenii Solovei <146028527+TheDidEva@users.noreply.github.com>
2024-01-14 19:46:52 +0200

    continue lern, make one more contarct ./2 git commit --amend

---
+++ 2/contracts/Lottery.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+contract Lottery {
+    address public manager;
+    address payable[] public players;
+
+    constructor() public {
+        manager = msg.sender; //the address of the user who called the instance of the contract
+    }
+
+    function enter() public payable {
+        //payable - we nee pay(eth) whene we will use this function
+
+        //this is boolean exoression, if return false - exit from funciton
+        //in this example we cheking, that the value more then 0.01 etherium(on user balance)
+        require(msg.value > .01 ether);
+        players.push(payable(msg.sender)); //the addres of the user who called this function
+    }
+
+    function random() private view returns (uint256) {
+        //sha3, keccak256 - hashing algo
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.difficulty, block.timestamp, players)
+                )
+            ); //abi.encodePacked - for packin in one byte array, becose keccak256 - can accept one argument
+    }
+
+    function pickWiner() public {
+        require(msg.sender == manager); //ніхто окрім менеджера не зможе викликати цю функцію
+
+        uint256 index = random() % players.length;
+        address payable winner = players[index];
+
+        // Отримання балансу контракту і використання address(this).balance
+        winner.transfer(address(this).balance);
+
+        // Очистіть список гравців після визначення переможця
+        players = new address payable[](0);
+    }
+}
